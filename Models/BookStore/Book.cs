a3f2ade058c1f2bae63a68c00da763a2194202af
serezmatik <57442528+serezmatik@users.noreply.github.com>
2023-09-13 23:29:31 +0200

    Dodaj pliki projektów.

---
+++ Models/BookStore/Book.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AdvancedProgramming_Lesson1.Models.BookStore
+{
+    public class Book
+    {
+        [Display(Name = "#")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nazwa książki jest wymagana")]
+        [Display(Name = "Tytuł")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Rok wydania jest wymagany")]
+        [Range(1, int.MaxValue, ErrorMessage = "Nieprawidłowy rok wydania")]
+        [Display(Name = "Rok wydania")]
+        public int YearOfRelease { get; set; }
+        [Required(ErrorMessage = "Gatunek jest wymagany")]
+        [Display(Name = "Gatunek")]
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+        [Required(ErrorMessage = "Autor jest wymagany")]
+        [Display(Name = "Autor")]
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+        [Required(ErrorMessage = "Ilość stron jest wymagana")]
+        [Range(1, int.MaxValue, ErrorMessage = "Nieprawidłowa ilość stron")]
+        [Display(Name = "Ilość strona")]
+        public int PagesCount { get; set; }
+        [Required(ErrorMessage = "Cena jest wymagana")]
+        [Range(1, int.MaxValue, ErrorMessage = "Nieprawidłowa cena")]
+        [Column(TypeName = "decimal(6, 2)")]
+        [Display(Name = "Cena")]
+        public decimal ListPrice { get; set; }
+
+
+
+
+
+
+
+    }
+}
